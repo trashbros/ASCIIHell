a5849c0ad2caf41adc83036a1e64db1770e72a1f
Reakain <reakain@users.noreply.github.com>
2019-12-30 12:58:42 -0500

    Updated enemy to actually move aroun in preparation for enemy pathing

--- ASCII Hell/Assets/ASCII-Hell/Scripts/EnemyController.cs
+++ ASCII Hell/Assets/ASCII-Hell/Scripts/EnemyController.cs
@@ -10,9 +10,13 @@ public class EnemyController : MonoBehaviour, ICollidable
     [SerializeField] private bool m_gamePaused = false;
     [SerializeField] private bool m_gameSlowed = false;
 
+    Rigidbody2D m_rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
+        m_rigidbody2d = GetComponent<Rigidbody2D>();
+
         CustomEvents.EventUtil.AddListener(CustomEventList.PARAMETER_CHANGE, OnParameterChange);
         CustomEvents.EventUtil.AddListener(CustomEventList.GAME_PAUSED, OnGamePause);
         CustomEvents.EventUtil.AddListener(CustomEventList.SLOW_TIME, OnSlowTime);
@@ -25,6 +29,22 @@ public class EnemyController : MonoBehaviour, ICollidable
         {
             return;
         }
+
+        Vector2 move = GetMovement();
+
+        // Get your current position
+        Vector2 position = m_rigidbody2d.position;
+
+        // Set your position as your position plus your movement vector, times your speed multiplier, and the current game timestep;
+        position = position + move * m_speed * Time.deltaTime * (m_gameSlowed ? m_slowDownPercent : 1f);
+
+        // Tell the rigidbody to move to the positon specified
+        m_rigidbody2d.MovePosition(position);
+    }
+
+    private Vector2 GetMovement()
+    {
+        return Vector2.left;
     }
 
     public void OnHit(GameObject collision)
