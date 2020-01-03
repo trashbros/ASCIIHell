eb8da8ba9ad5306d52b37c02723e5bf42154f2fd
Reakain <reakain@users.noreply.github.com>
2020-01-03 14:37:02 -0500

    Smoother motion maybe???????

--- ASCII Hell/Assets/ASCII-Hell/Scripts/Entity.cs
+++ ASCII Hell/Assets/ASCII-Hell/Scripts/Entity.cs
@@ -19,6 +19,8 @@ public abstract class Entity : MonoBehaviour, ICollidable
 
     public bool IsAlive{ get{return m_alive;}}
 
+    private bool m_isMoving = false;
+
     // Start is called before the first frame update
     protected void Start()
     {
@@ -76,15 +78,29 @@ public abstract class Entity : MonoBehaviour, ICollidable
 
     protected void MoveEntity()
     {
+        if(m_isMoving)
+        {
+            return;
+        }
+
         Vector2 move = GetMovement();
+
+        if(move == Vector2.zero)
+        {
+            return;
+        }
 
         // Get your current position
         Vector2 position = this.transform.position;
 
         // Set your position as your position plus your movement vector, times your speed multiplier, and the current game timestep;
-        position = position + move * m_speed * Time.deltaTime * (!UseSlowDown ? 1f : (m_gameSlowed ? m_slowDownPercent : 1f));
+        var posAdd = move * m_speed * Time.deltaTime * (!UseSlowDown ? 1f : (m_gameSlowed ? m_slowDownPercent : 1f));
+
+        position += posAdd;
+        //position = position + new Vector2(Mathf.RoundToInt(posAdd.x), Mathf.RoundToInt(posAdd.y));
 
         this.transform.position = new Vector2(position.x, position.y);
+        //StartCoroutine(SmoothMovement(position));
     }
 
     protected abstract Vector2 GetMovement();
@@ -116,6 +132,38 @@ public abstract class Entity : MonoBehaviour, ICollidable
     }
 
     protected abstract void OnParameterChange(CustomEvents.EventArgs evt);
+    #endregion
+
+    #region Coroutines
+
+    //Co-routine for moving units from one space to next, takes a parameter end to specify where to move to.
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        m_isMoving = true;
+        //Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
+        //Square magnitude is used instead of magnitude because it's computationally cheaper.
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        //While that distance is greater than a very small amount (Epsilon, almost zero):
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            var inverseMoveTime = m_speed * Time.deltaTime * (!UseSlowDown ? 1f : (m_gameSlowed ? m_slowDownPercent : 1f));
+
+            //Find a new position proportionally closer to the end, based on the moveTime
+            Vector3 newPostion = Vector3.MoveTowards(transform.position, end, inverseMoveTime);
+
+            //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+            transform.position = new Vector2(newPostion.x, newPostion.y);
+
+            //Recalculate the remaining distance after moving.
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            //Return and loop until sqrRemainingDistance is close enough to zero to end the function
+            yield return null;
+        }
+        m_isMoving = false;
+    }
+
     #endregion
 
     protected void OnDestroy()
